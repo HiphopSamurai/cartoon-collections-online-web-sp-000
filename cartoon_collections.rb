3c13a5b7fb3609a03419ef99fb82f75805c91633
Garrett Thompson <gtcruz.gt@gmail.com>
2019-07-19 03:52:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  i = 0
+  x = 1
+  while i < array.length
+    puts "#{x}. #{array[i]}"
+    i += 1
+    x += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_array = []
+  i = 0
+  while i < array.length
+    new_array << array[i].capitalize + '!'
+    i += 1
+  end
+  return new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  if array.any? { |word| word.length > 4 }
+    return true
+  else
+    return false
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
